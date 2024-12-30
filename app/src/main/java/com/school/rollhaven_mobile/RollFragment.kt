267574e9649eb1e281d7fce799c1aa5d7709a6e1
Litneg <sean.descamps.7@gmail.com>
2024-12-30 19:58:21 +0100

    debut roll

---
+++ app/src/main/java/com/school/rollhaven_mobile/RollFragment.kt
@@ -0,0 +1,72 @@
+package com.school.rollhaven_mobile
+
+import android.R
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import com.school.rollhaven_mobile.databinding.FragmentRollBinding
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Use the [RollFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class RollFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var _binding: FragmentRollBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentRollBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Configure les adaptateurs des spinners
+        val typeRolls = listOf("Combat", "Magie", "Competence", "Autre")
+        val adapter = ArrayAdapter(
+            requireContext(),
+            R.layout.simple_spinner_item,
+            typeRolls
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        binding.spinnerType.adapter = adapter
+
+        binding.spinnerType?.onItemSelectedListener= object :AdapterView.OnItemSelectedListener{
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                Toast.makeText(this@RollFragment,"item is ${typeRolls[position]}", Toast.LENGTH_LONG).show()
+            }
+        }
+
+
+
+        binding.spinnerAutre.adapter = adapter
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
